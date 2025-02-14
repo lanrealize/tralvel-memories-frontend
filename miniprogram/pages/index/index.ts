d47e381682fd1956f89331cb9c6d983b93d33698
Sun Peng Yuan <sxtyspy@126.com>
2025-02-14 19:06:39 +0800

    Try to use mobx correctly

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -1,25 +1,63 @@
 // index.ts
 import { createStoreBindings } from 'mobx-miniprogram-bindings';
 import { generalStore } from '../../stores/generalStore';
+import { albumsStore } from '../../stores/albumsStore';
+import { getAlbums } from '../../utils/apis';
 
 Page({
 
-  storageBinding: undefined as any,
+  generalStorageBinding: undefined as any,
+  albumsStorageBinding: undefined as any,
 
   data: {
     
   },
 
-  methods: {
+  async onLoad() {
+
+    this.generalStorageBinding = createStoreBindings(this, 
+      {
+        store: generalStore,
+        fields: ['loginStatus'],
+        actions: ['setLoginStatus']
+      }
+    );
+
+    this.albumsStorageBinding = createStoreBindings(this, 
+      {
+        store: albumsStore,
+        fields: ['albums'],
+        actions: ['setAlbums']
+      }
+    );
+
+
+    await this.updateAlubms();
+    // Promise.resolve().then(() => {
+    //   this.updateAlubms();
+    //   console.log(this.data);
+    // });
+
+    setTimeout(() => {
+      console.log(this.data.albums)
+    }, 0);
 
   },
 
-  onLoad() {
-    this.storageBinding = createStoreBindings(this, {
-      store: generalStore,
-      fields: ['loginStatus'],
-      actions: {setLoginStatus: 'setLoginStatus'}
-    });
+  onUnload() {
+    this.generalStorageBinding.destroy();
+    this.albumsStorageBinding.destroy();
+  },
+
+  async updateAlubms() {
+    try {
+      const albumList = await getAlbums();
+      (this as any).setAlbums(albumList);
+      this.albumsStorageBinding.updateStoreBindings();
+      console.log((this as any).data.albums);
+    } catch (e) {
+      throw (e);
+    }
   }
 
 })
