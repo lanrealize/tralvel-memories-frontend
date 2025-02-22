5344c36310c5e26133a19595650218f744efbb1a
Sun Peng Yuan <sxtyspy@126.com>
2025-02-22 19:31:39 +0800

    added photo component

--- miniprogram/components/photo/photo.ts
+++ miniprogram/components/photo/photo.ts
@@ -5,20 +5,75 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    photoId: {
+      type: String,
+      value: undefined
+    },
+    title: {
+      type: String,
+      value: undefined
+    },
+    subTitle: {
+      type: String,
+      value: undefined
+    },
+    location: {
+      type: String,
+      value: undefined
+    },
+    description: {
+      type: String,
+      value: undefined
+    },
+    photoUrl: {
+      type: String,
+      value: undefined
+    },
+    type: {
+      type: String,
+      value: undefined
+    },
+    photoUrls: {
+      type: Array,
+      value: []
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    opacity: 0
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    previewImage() {
+      wx.previewImage({
+        current: this.properties.photoUrl,
+        urls: this.properties.photoUrls
+      });
+    },
 
+    onLongPress() {
+      this.setOpacity(1);
+    },
+
+    setOpacity(opacity: number) {
+      this.setData({
+        opacity: opacity
+      })
+    },
+
+    onCoverClick() {
+      this.setOpacity(0);
+    },
+
+    async onDeleteClick() {
+      // await deletePhoto(this.properties.photoId);
+      this.setOpacity(0);
+    }
   }
 })
--- miniprogram/pages/photos/photos.ts
+++ miniprogram/pages/photos/photos.ts
@@ -12,7 +12,11 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad() {
-
+    const app: IAppOption = getApp();
+    this.setData({
+      menuHeight: app.globalData.navigationInfo.menuHeight,
+      menuTop: app.globalData.navigationInfo.menuTop,
+    })
   },
 
   /**
