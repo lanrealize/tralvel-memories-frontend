ab416c7b824ed142a8425e1d93da3aa9fc4c6b57
Sun Peng Yuan <sxtyspy@126.com>
2025-02-28 00:27:11 +0800

    add backend && adjust timeline

--- miniprogram/configs/network.ts
+++ miniprogram/configs/network.ts
@@ -1,2 +1,3 @@
 export const urlPrefix = 'https://www.lifeprint.site/api/mtm'
-export const devUrlPrefix = 'http://192.168.0.114:3000/api/mtm'
+// export const devUrlPrefix = 'http://192.168.0.114:3000/api/mtm'
+export const devUrlPrefix = 'https://www.wltech-service.site/api/mtm'
