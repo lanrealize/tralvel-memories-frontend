dfc6fc193c6a71108d1908da80bd02803dd44c78
Sun Peng Yuan <sxtyspy@126.com>
2025-03-03 15:55:50 +0800

    revert photos change

--- miniprogram/pages/photos/photos.ts
+++ miniprogram/pages/photos/photos.ts
@@ -19,8 +19,7 @@ Page({
     subTitle: '',
     loading: false,
     threshold: 0,
-    photoCreationComponentTop: 100,
-    photoDisplayTarget: ''
+    photoCreationComponentTop: 100
   },
 
   /**
@@ -156,12 +155,6 @@ Page({
 
   receivePhotoDelete() {
     // this.setFadeInOut();
-  },
-
-  test() {
-    this.setData({
-      photoDisplayTarget: 'photos--1'
-    });
   }
 
 })
