a9a6fc28fa867e95ca916e89dde23acf5ab08ac2
Sun Peng Yuan <sxtyspy@126.com>
2025-05-15 09:15:53 +0800

    add component - photos-swiper-indicator

---
+++ miniprogram/components/photos-swiper-indicator/photos-swiper-indicator.ts
@@ -0,0 +1,24 @@
+// components/photos-swiper-indicator/photos-swiper-indicator.ts
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
