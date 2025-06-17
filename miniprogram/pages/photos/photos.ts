cb23b28e8d86a901b30ad4c10c7b9f3f23b42be1
Sun Peng Yuan <sxtyspy@126.com>
2025-06-17 21:23:14 +0800

    remove onshow

--- miniprogram/pages/photos/photos.ts
+++ miniprogram/pages/photos/photos.ts
@@ -125,8 +125,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow() {
-    (this as any).setPhotoIsSwitching(false);
-    this.manageDisplyedImgaeAnimation(false, (this as any).data.photoDisplayIndex);
+    
   },
 
   /**
