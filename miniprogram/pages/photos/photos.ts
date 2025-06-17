4348f8f8f166741e9583d01de29cdd21300a446f
Sun Peng Yuan <sxtyspy@126.com>
2025-06-17 17:08:28 +0800

    on show event

--- miniprogram/pages/photos/photos.ts
+++ miniprogram/pages/photos/photos.ts
@@ -125,7 +125,8 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow() {
-    
+    (this as any).setPhotoIsSwitching(false);
+    this.manageDisplyedImgaeAnimation(false, (this as any).data.photoDisplayIndex);
   },
 
   /**
