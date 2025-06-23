7c276b1ba1f09ed9cbf6c5cc67a55f895a7a0096
Sun Peng Yuan <sxtyspy@126.com>
2025-06-23 22:49:59 +0800

    white screen

--- miniprogram/pages/photos/photos.ts
+++ miniprogram/pages/photos/photos.ts
@@ -115,20 +115,6 @@ Page({
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-    
-  },
-
-  /**
    * 生命周期函数--监听页面隐藏
    */
   onHide() {
@@ -145,20 +131,6 @@ Page({
     this.photosStorageBinding?.destroyStoreBindings();
     this.photoCreationStoreBinding?.destroyStoreBindings();
     this.pagesStorageBinding?.destroyStoreBindings();
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
   },
 
   /**
@@ -215,9 +187,13 @@ Page({
 
   showAppearAnimation() {
     this.setData({
-      showAppearAnimation: true,
-      apearAnimationClass: 'active'
+      showAppearAnimation: true
     });
+    setTimeout(() => {
+      this.setData({
+        apearAnimationClass: 'active'
+      });
+    }, 500);
   },
 
   adjustMenubarPosition() {
@@ -316,9 +292,8 @@ Page({
         });
         setTimeout(() => {
           this.manageDisplyedImgaeAnimation(false, (this as any).data.photoDisplayIndex);
-
-        }, 1500);
-      }, 4000);
+        }, 1000);
+      }, 3500);
     } else {
       setTimeout(() => {
         this.setData({
